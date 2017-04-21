e06796ca466213972ccdc9c6c609bc32d8863b2a
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-21 15:39:18 +0300

    Adjusted java Approval class constructor to account for JWT

--- src/main/java/ee/ria/riha/models/Approval.java
+++ src/main/java/ee/ria/riha/models/Approval.java
@@ -4,11 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class Approval {
+  String JWT;
   String uri;
   String timestamp;
   String status;
 
-  public Approval(String uri, String timestamp, String status) {
+  public Approval(String JWT, String uri, String timestamp, String status) {
+	this.JWT = JWT;
     this.uri = uri;
     this.timestamp = timestamp;
     this.status = status;
