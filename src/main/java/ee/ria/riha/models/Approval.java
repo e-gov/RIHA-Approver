99b9516a44a7e5c71c7d60760fbac41103fc9771
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-27 15:08:13 +0300

    Revert "Added a token field into the constructor and also into the requestmap"
    This reverts commit 6353a066f220834d17195e60edb802900e9da18f.

--- src/main/java/ee/ria/riha/models/Approval.java
+++ src/main/java/ee/ria/riha/models/Approval.java
@@ -7,12 +7,10 @@ public class Approval {
   String uri;
   String timestamp;
   String status;
-  string token;
 
-  public Approval(String uri, String timestamp, String status, String token) {
+  public Approval(String uri, String timestamp, String status) {
     this.uri = uri;
     this.timestamp = timestamp;
     this.status = status;
-    this.token = token;
   }
 }
