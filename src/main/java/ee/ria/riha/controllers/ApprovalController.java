ece963e667e0a898f1db0c19f50eec301ea9918b
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-27 14:34:07 +0300

    Changed the import to include everything arraylist needs

--- src/main/java/ee/ria/riha/controllers/ApprovalController.java
+++ src/main/java/ee/ria/riha/controllers/ApprovalController.java
@@ -1,6 +1,6 @@
 package ee.ria.riha.controllers;
 
-import java.util.ArrayList;
+import java.util.*;
 import ee.ria.riha.models.Approval;
 import ee.ria.riha.models.Status;
 import ee.ria.riha.services.ApprovalStorageService;
