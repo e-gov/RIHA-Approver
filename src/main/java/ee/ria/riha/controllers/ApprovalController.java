bde240ce84f8e2111b50f4b007bffbebe9050bbd
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-27 13:32:59 +0300

    Added import for requestHeader

--- src/main/java/ee/ria/riha/controllers/ApprovalController.java
+++ src/main/java/ee/ria/riha/controllers/ApprovalController.java
@@ -12,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
