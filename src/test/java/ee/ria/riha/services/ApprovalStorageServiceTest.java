cfbd43af3e999bb09b53b757076ac0d101467690
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-04-27 15:20:37 +0300

    Updated ApprovalStorageServiceTest's to match new approval constructor

--- src/test/java/ee/ria/riha/services/ApprovalStorageServiceTest.java
+++ src/test/java/ee/ria/riha/services/ApprovalStorageServiceTest.java
@@ -48,9 +48,9 @@ public class ApprovalStorageServiceTest {
 
   @Test
   public void approvedApprovals() {
-    Approval approved1 = new Approval("http://base.url/shortname-2", "2015-10-10T01:10:10", "KOOSKÕLASTATUD");
-    Approval notApproved = new Approval("http://base.url/shortname-1", "2016-01-01T10:00:00", "MITTE KOOSKÕLASTATUD");
-    Approval approved2 = new Approval("http://base.url/shortname-3", "2014-01-01T10:00:00", "KOOSKÕLASTATUD");
+    Approval approved1 = new Approval("http://base.url/shortname-2", "2015-10-10T01:10:10", "KOOSKÕLASTATUD", "testJWT");
+    Approval notApproved = new Approval("http://base.url/shortname-1", "2016-01-01T10:00:00", "MITTE KOOSKÕLASTATUD", "testJWT");
+    Approval approved2 = new Approval("http://base.url/shortname-3", "2014-01-01T10:00:00", "KOOSKÕLASTATUD", "testJWT");
     doReturn(asList(approved1, notApproved, approved2)).when(service).allApprovals();
 
     List<Approval> result = service.approvedApprovals();
@@ -60,7 +60,7 @@ public class ApprovalStorageServiceTest {
 
   @Test
   public void saveInfosystemApproval_noExistingFile() throws IOException {
-    service.saveInfosystemApproval(new Approval("http://base.url/infosystem-name","2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD"));
+    service.saveInfosystemApproval(new Approval("http://base.url/infosystem-name","2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD", "testJWT"));
 
     assertEquals("2016-12-12T08:05:08.4567|MITTE KOOSKÕLASTATUD", approvals().getProperty("http://base.url/infosystem-name"));
   }
@@ -71,7 +71,7 @@ public class ApprovalStorageServiceTest {
     existingApprovals.setProperty("http://base.url/other-infosystem-name", "2016-12-12T01:01:01|KOOSKÕLASTATUD");
     existingApprovals.store(Files.newOutputStream(storageFilePath), null);
 
-    service.saveInfosystemApproval(new Approval("http://base.url/infosystem-name","2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD"));
+    service.saveInfosystemApproval(new Approval("http://base.url/infosystem-name","2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD", "testJWT"));
 
     Properties approvals = approvals();
     assertEquals(2, approvals.size());
@@ -85,7 +85,7 @@ public class ApprovalStorageServiceTest {
     for (int i = 0; i < 10; i++) {
       Thread thread = new Thread(() -> {
         try {
-          service.saveInfosystemApproval(new Approval(Thread.currentThread().getName(),"2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD"));
+          service.saveInfosystemApproval(new Approval(Thread.currentThread().getName(),"2016-12-12T08:05:08.4567", "MITTE KOOSKÕLASTATUD", "testJWT"));
         }
         catch (Throwable e) {
           e.printStackTrace();
