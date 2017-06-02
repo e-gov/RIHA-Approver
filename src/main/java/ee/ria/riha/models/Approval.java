067b1c7ad70998e66522c5af3fe223dec6edde6a
TaaviMeinberg <taavi.meinberg@gmail.com>
2017-06-02 09:10:04 +0300

    Added model file for Approval.java which was missed in the initial adding

--- src/main/java/ee/ria/riha/models/Approval.java
+++ src/main/java/ee/ria/riha/models/Approval.java
@@ -7,10 +7,14 @@ public class Approval {
   String uri;
   String timestamp;
   String status;
+  String token;
+  String comment;
 
-  public Approval(String uri, String timestamp, String status) {
+  public Approval(String uri, String timestamp, String status, String token, String comment) {
     this.uri = uri;
     this.timestamp = timestamp;
     this.status = status;
+    this.token = token;
+    this.comment = comment;
   }
 }
