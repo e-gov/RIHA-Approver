1b7f317bafe17f252df3c4aca19368ba77405922
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-07-03 09:29:43 +0300

    RIHAKB-141. Remove obsolete code

--- src/main/java/ee/ria/riha/ApproverApplication.java
+++ src/main/java/ee/ria/riha/ApproverApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApproverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApproverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApproverApplication.class, args);
+    }
 }
---
+++ src/main/java/ee/ria/riha/web/ApprovalController.java
@@ -0,0 +1,9 @@
+package ee.ria.riha.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ApprovalController {
+
+
+}
