5fc75eaf045508dc3a2083c305ab4555ea56f8a7
Valentin Suhnjov <valentin.suhnjov@wisercat.eu>
2017-08-21 17:45:29 +0300

    RIHAKB-247. Enable CORS

--- src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
+++ src/main/java/ee/ria/riha/conf/WebMvcConfiguration.java
@@ -2,10 +2,12 @@ package ee.ria.riha.conf;
 
 import ee.ria.riha.storage.util.FilterableArgumentResolver;
 import ee.ria.riha.storage.util.PageableArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -14,11 +16,23 @@ import java.util.UUID;
 @Configuration
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    @Autowired
+    private ApplicationProperties applicationProperties;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         super.addArgumentResolvers(argumentResolvers);
         argumentResolvers.add(new PageableArgumentResolver());
         argumentResolvers.add(new FilterableArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        applicationProperties.getSecurity().getCors().getAllowedOrigins().toArray(new String[0]))
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 
     @Override
